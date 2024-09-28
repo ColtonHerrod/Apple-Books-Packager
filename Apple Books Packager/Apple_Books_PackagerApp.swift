cd76cdcac10b3da48c96a259b1840238dbd4749e
Colton Herrod <colton@herrod.net>
2024-09-28 15:33:36 -0500

    Initial Commit

---
+++ Apple Books Packager/Apple_Books_PackagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Books_PackagerApp.swift
+//  Apple Books Packager
+//
+//  Created by Colton Herrod on 9/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Books_PackagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple Books PackagerUITests/Apple_Books_PackagerUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Apple_Books_PackagerUITestsLaunchTests.swift
+//  Apple Books PackagerUITests
+//
+//  Created by Colton Herrod on 9/28/24.
+//
+
+import XCTest
+
+final class Apple_Books_PackagerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
